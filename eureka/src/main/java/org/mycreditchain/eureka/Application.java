da317957352d344a4acab634d18f03c1dfdfe027
james.lee <james.lee@finger.co.kr>
2018-08-23 18:10:16 +0900

    테스트 설정 제거

--- eureka/src/main/java/org/mycreditchain/eureka/Application.java
+++ eureka/src/main/java/org/mycreditchain/eureka/Application.java
@@ -3,27 +3,12 @@ package org.mycreditchain.eureka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-import de.codecentric.boot.admin.server.config.EnableAdminServer;
 
 @EnableEurekaServer
 @SpringBootApplication
-@EnableAdminServer
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-	
-	@Configuration
-    public static class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter {
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
-            http.authorizeRequests().anyRequest().permitAll()  
-                .and().csrf().disable();
-        }
-    }
 }
